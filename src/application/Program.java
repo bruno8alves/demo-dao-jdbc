f9382f9d836701deb8b895d86f022310ad3420f3
Bruno Alves <bruno.8.alves@gmail.com>
2022-06-13 15:04:16 -0700

    update implementation.

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,7 +39,11 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted new Id= " + newSeller.getId());
 		
-		
+		System.out.println("=== TEST 5: seller update===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
